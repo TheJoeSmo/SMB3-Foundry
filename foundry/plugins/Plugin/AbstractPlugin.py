eb4efd9c17253e9f59801bb8ecaca5200c25dc9e
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-01-02 14:48:51 -0500

    Create AbstractPlugin.py

---
+++ foundry/plugins/Plugin/AbstractPlugin.py
@@ -0,0 +1,40 @@
+
+
+from abc import abstractmethod
+
+
+class AbstractPlugin:
+    """
+    An interface of what a Plugin should be
+    """
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    @abstractmethod
+    def create(self) -> bool:
+        """
+        Creates any extra dependencies required for the plugin.
+        :return: a bool depending if it created these dependencies successfully
+        """
+
+    @abstractmethod
+    def delete(self) -> bool:
+        """
+        Delete itself and any dependencies that will not be needed
+        :return: a bool depending if it deleted these dependencies successfully
+        """
+
+    @abstractmethod
+    def enable(self) -> bool:
+        """
+        Enables itself and handles any other requirements that are needed
+        :return: a bool depending if it enabled itself successfully
+        """
+
+    @abstractmethod
+    def disabled(self) -> bool:
+        """
+        Disable itself and handles any other disabling required
+        :return: a bool depending if it disabled itself successfully
+        """
