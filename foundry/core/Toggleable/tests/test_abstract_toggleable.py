0eebc36e99db2f5e91c9b0b0763a4f710f29db6b
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-01-05 21:35:03 -0500

    Add state updating enable/disable tests

--- foundry/core/Toggleable/tests/test_abstract_toggleable.py
+++ foundry/core/Toggleable/tests/test_abstract_toggleable.py
@@ -8,9 +8,11 @@ from foundry.core.Toggleable.AbstractToggleable import AbstractToggleable
 
 class GenericToggleable(AbstractToggleable):
     def enable(self, *args, **kwargs) -> bool:
+        self.value = True
         return True
 
     def disable(self, *args, **kwargs) -> bool:
+        self.value = False
         return True
 
 
@@ -24,6 +26,11 @@ def test_uninitialized_state_exception():
         _ = toggleable.state
 
 
+def test_setting_state_from_init():
+    toggleable = GenericToggleable(True)
+    assert toggleable._state
+
+
 def test_setting_state():
     toggleable = GenericToggleable()
     toggleable.state = True
@@ -31,7 +38,16 @@ def test_setting_state():
 
 
 def test_getting_state():
-    toggleable = GenericToggleable()
-    toggleable.state = True
+    toggleable = GenericToggleable(True)
     toggleable.state = not toggleable.state
     assert not toggleable.state
+
+
+def test_state_enable():
+    toggleable = GenericToggleable(True)
+    assert toggleable.value
+
+
+def test_state_disable():
+    toggleable = GenericToggleable(False)
+    assert not toggleable.value
