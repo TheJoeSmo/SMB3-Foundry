0bdc4fef8e5d71a6875175101326542bf5d230cf
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-01-07 14:16:03 -0500

    Fix to make observer for each instance

--- foundry/core/Action/util/action_decorator.py
+++ foundry/core/Action/util/action_decorator.py
@@ -14,18 +14,32 @@ class ActionDecorator:
     def __init__(self, func: Callable, action_name: str, observable_name: str):
         update_wrapper(self, func)
         self.func = func
-        observer = Observable(name=observable_name)
-        self.action = Action(name=action_name, observer=observer)
+        self.observable_name = observable_name
+        self.action_name = action_name
+        self.action = None
 
     def __get__(self, instance, owner):
+        self.create_action(instance)
         func = partial(self.__call__, instance)
-        func.action = self.action  # make the observable visible through get
         return func
 
     def __call__(self, instance, *args, **kwargs):
+        self.create_action(instance)
         result = self.func(instance, *args, **kwargs)
-        self.action(result=result)
+        instance._actions[self.action_name](result=result)
         return result
+
+    def create_action(self, instance):
+        """
+        Generates an action for the instance of the class
+        :return: an action
+        """
+        if not hasattr(instance, "_actions"):
+            instance._actions = {}
+        if self.action_name not in instance._actions:
+            observer = Observable(name=self.observable_name)
+            self.action = Action(name=self.action_name, observer=observer)
+            instance._actions.update({self.action_name: self.action})
 
 
 def action_decorator(action_name: str, observable_name: str):
