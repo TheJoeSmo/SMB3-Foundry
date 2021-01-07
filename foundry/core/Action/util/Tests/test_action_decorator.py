838ef8eff8206d4a9de45d5a5fa8247cb28f321f
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-01-07 00:12:18 -0500

    Create test for action decorator

---
+++ foundry/core/Action/util/Tests/test_action_decorator.py
@@ -0,0 +1,49 @@
+
+
+import pytest
+
+from foundry.core.Action.util.action_decorator import action_decorator
+from foundry.core.Action.Action import Action
+
+
+class TestClass:
+    def __init__(self):
+        self.value = 0
+
+    @action_decorator("add_one", "add_one")
+    def add_one(self) -> int:
+        self.value += 1
+        return self.value
+
+    @action_decorator("add_value", "add_value")
+    def add_value(self, value: int = 1) -> int:
+        self.value += value
+        return self.value
+
+
+def test_initialization():
+    TestClass()
+
+
+def test_simple_method_call():
+    test = TestClass()
+    assert test.add_one() == 1
+    assert test.value == 1
+
+
+def test_complex_method_call():
+    test = TestClass()
+    assert test.add_value(5) == 5
+    assert test.value == 5
+
+
+def test_observer():
+    test, copy = TestClass(), TestClass()
+    action: Action = test.add_one.action
+    action.observer.attach_observer(lambda value: setattr(copy, "value", value))
+    test.add_one()
+    assert copy.value == 1
+    test.add_value(5)
+    assert copy.value == 1
+    test.add_one()
+    assert copy.value == 7
