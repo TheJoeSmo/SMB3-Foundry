2db39ca8fffdfcd37e184690f6c561165da7d732
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-01-07 15:07:38 -0500

    Update to use ActionObject format

--- foundry/core/Action/util/smart_action_decorator.py
+++ foundry/core/Action/util/smart_action_decorator.py
@@ -3,6 +3,8 @@
 from typing import Callable
 from functools import partial
 
+from foundry.core.Observables.Observable import Observable
+from foundry.core.Action.Action import Action
 from foundry.core.Action.util.action_decorator import ActionDecorator as ActionDecorator
 
 
@@ -13,6 +15,7 @@ class SmartActionDecorator(ActionDecorator):
 
     def __init__(self, func: Callable, action_name: str, observable_name: str):
         super().__init__(func, action_name, observable_name)
+        self.reference_name = f"{action_name}_action"
 
     def __get__(self, instance, owner):
         if not hasattr(instance, "_actions_enabled"):
@@ -27,12 +30,17 @@ class SmartActionDecorator(ActionDecorator):
 
     def __call__(self, instance, *args, **kwargs):
         result = self.func(instance, *args, **kwargs)
-        instance._actions[self.action_name](result=result)
+        instance._actions[self.reference_name](result=result)
         return result
 
     def create_action(self, instance):
         instance._actions_enabled[self.action_name] = True
-        super().create_action(instance)
+        if not hasattr(instance, "_actions"):
+            instance._actions = {}
+        if self.action_name not in instance._actions:
+            observer = Observable(name=self.observable_name)
+            self.action = Action(name=self.action_name, observer=observer)
+            instance._actions.update({self.reference_name: self.action})
 
 
 def smart_action_decorator(action_name: str, observable_name: str):
