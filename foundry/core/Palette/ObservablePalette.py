ba68eb56321ce95888050bd7cf2465215ea08a0b
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-02-15 23:37:45 -0500

    Create Palette

---
+++ foundry/core/Palette/ObservablePalette.py
@@ -0,0 +1,92 @@
+
+
+from copy import copy
+
+from ..Observables.GenericObservable import GenericObservable
+from ..Color.Color import Color
+from ..Color.ObservableColor import ObservableColor
+from Palette import Palette
+
+
+def _get_color(palette: Palette, index: int) -> Color:
+    return palette[index]
+
+
+def _set_color(palette: Palette, index: int, color: Color):
+    palette[index] = color
+
+
+class ObservablePalette(Palette):
+    """
+    A palette that emits an update when edited
+    """
+
+    def __init__(self, color_0: Color, color_1: Color, color_2: Color, color_3: Color):
+        self._palette = Palette(
+            ObservableColor.from_color(color_0),
+            ObservableColor.from_color(color_1),
+            ObservableColor.from_color(color_2),
+            ObservableColor.from_color(color_3)
+        )
+        self.update_action = GenericObservable("palette_update")
+
+    def __str__(self) -> str:
+        return self._palette.__str__()
+
+    def __getitem__(self, item: int) -> Color:
+        return self.palette[item]
+
+    def __setitem__(self, key: int, value: Color):
+        pal = self.palette  # This is a copy of the actual palette being edited
+        pal[key] = value
+        self.palette = pal
+
+    @property
+    def nes_str(self) -> str:
+        """Returns the color as a NES string"""
+        return self._palette.nes_str
+
+    @property
+    def color_0(self) -> Color:
+        return _get_color(self, 0)
+
+    @color_0.setter
+    def color_0(self, color: Color):
+        _set_color(self, 0, color)
+
+    @property
+    def color_1(self) -> Color:
+        return _get_color(self, 1)
+
+    @color_1.setter
+    def color_1(self, color: Color):
+        _set_color(self, 1, color)
+
+    @property
+    def color_2(self) -> Color:
+        return _get_color(self, 2)
+
+    @color_2.setter
+    def color_2(self, color: Color):
+        _set_color(self, 2, color)
+
+    @property
+    def color_3(self) -> Color:
+        return _get_color(self, 3)
+
+    @color_3.setter
+    def color_3(self, color: Color):
+        _set_color(self, 3, color)
+
+    @property
+    def palette(self) -> Palette:
+        """
+        This provides a full copy of the palette.  Even the colors will not refer to the actual palette.
+        :return:
+        """
+        return Palette(copy(self._palette[0]), copy(self._palette[1]), copy(self._palette[2]), copy(self._palette[3]))
+
+    @palette.setter
+    def palette(self, palette: Palette) -> None:
+        self._palette = palette
+        self.update_action(palette)
---
+++ foundry/core/Palette/Palette.py
@@ -0,0 +1,46 @@
+
+
+from dataclasses import dataclass
+
+from ..Color.Color import Color
+
+
+@dataclass
+class Palette:
+    """Defines a basic palette"""
+    color_0: Color
+    color_1: Color
+    color_2: Color
+    color_3: Color
+
+    def __str__(self) -> str:
+        return f"({self[0]}),({self[1]}),({self[2]}),({self[3]})"
+
+    @property
+    def nes_str(self) -> str:
+        """Defines the palette as a NES palette string"""
+        return f"{self[0].nes_str},{self[1].nes_str},{self[2].nes_str},{self[3].nes_str}"
+
+    def __getitem__(self, item: int) -> Color:
+        if item == 0:
+            return self.color_0
+        elif item == 1:
+            return self.color_1
+        elif item == 2:
+            return self.color_2
+        elif item == 3:
+            return self.color_3
+        else:
+            raise NotImplementedError
+
+    def __setitem__(self, key: int, value: Color):
+        if key == 0:
+            self.color_0 = value
+        elif key == 1:
+            self.color_1 = value
+        elif key == 2:
+            self.color_2 = value
+        elif key == 3:
+            self.color_3 = value
+        else:
+            raise NotImplementedError
