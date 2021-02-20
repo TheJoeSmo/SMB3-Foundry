7c360fda426d8fbcf7ca6b673439d105635b5114
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-02-20 02:52:45 -0500

    Update ObservablePalette.py

--- foundry/core/Palette/ObservablePalette.py
+++ foundry/core/Palette/ObservablePalette.py
@@ -1,14 +1,12 @@
 
 
-from copy import copy
-
 from ..Observables.GenericObservable import GenericObservable
 from ..Color.Color import Color
 from ..Color.ObservableColor import ObservableColor
 from Palette import Palette
 
 
-def _get_color(palette: Palette, index: int) -> Color:
+def _get_color(palette: Palette, index: int) -> ObservableColor:
     return palette[index]
 
 
@@ -34,11 +32,13 @@ class ObservablePalette(Palette):
         return self._palette.__str__()
 
     def __getitem__(self, item: int) -> Color:
-        return self.palette[item]
+        #  We provide ObservableColors to the actual palette, which will be returned
+        #  This provides an entire copy, to protect the palette from any external changes
+        return self._palette[item].color
 
     def __setitem__(self, key: int, value: Color):
         pal = self.palette  # This is a copy of the actual palette being edited
-        pal[key] = value
+        pal[key] = ObservableColor.from_color(value)
         self.palette = pal
 
     @property
@@ -48,7 +48,7 @@ class ObservablePalette(Palette):
 
     @property
     def color_0(self) -> Color:
-        return _get_color(self, 0)
+        return _get_color(self, 0).color
 
     @color_0.setter
     def color_0(self, color: Color):
@@ -56,7 +56,7 @@ class ObservablePalette(Palette):
 
     @property
     def color_1(self) -> Color:
-        return _get_color(self, 1)
+        return _get_color(self, 1).color
 
     @color_1.setter
     def color_1(self, color: Color):
@@ -64,7 +64,7 @@ class ObservablePalette(Palette):
 
     @property
     def color_2(self) -> Color:
-        return _get_color(self, 2)
+        return _get_color(self, 2).color
 
     @color_2.setter
     def color_2(self, color: Color):
@@ -72,7 +72,7 @@ class ObservablePalette(Palette):
 
     @property
     def color_3(self) -> Color:
-        return _get_color(self, 3)
+        return _get_color(self, 3).color
 
     @color_3.setter
     def color_3(self, color: Color):
@@ -84,7 +84,7 @@ class ObservablePalette(Palette):
         This provides a full copy of the palette.  Even the colors will not refer to the actual palette.
         :return:
         """
-        return Palette(copy(self._palette[0]), copy(self._palette[1]), copy(self._palette[2]), copy(self._palette[3]))
+        return Palette(self._palette[0], self._palette[1], self._palette[2], self._palette[3])
 
     @palette.setter
     def palette(self, palette: Palette) -> None:
