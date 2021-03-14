488410c32ef8a466d1feb8fc33401bac34e7f708
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-03-14 13:42:19 -0400

    Create ColorButton

---
+++ foundry/gui/Color/ColorButton.py
@@ -0,0 +1,52 @@
+from typing import Optional
+from PySide2.QtWidgets import QToolButton, QWidget
+from PySide2.QtCore import QSize
+
+from foundry.core.Observables.GenericObservable import GenericObservable
+from foundry.core.Color.Color import Color
+from foundry.core.Color.ObservableColor import ObservableColor
+
+
+class ColorButton(QToolButton):
+    """A generic tool button with extended functionality"""
+    def __init__(self, parent: Optional[QWidget], color: Color):
+        super().__init__(parent)
+        self._color = ObservableColor(color.red, color.green, color.blue)
+
+        self.clicked_action = GenericObservable("clicked")
+        self.pressed_action = GenericObservable("pressed")
+        self.released_action = GenericObservable("released")
+        self.update_action = GenericObservable("update")
+
+        # Update the background and send updates upstream
+        self._color.update_action.attach_observer(
+            lambda c: self.setStyleSheet(f"background-color:rgb({c.red},{c.green},{c.blue}")
+        )
+        self._color.update_action.attach_observer(
+            lambda c: self.update_action.notify_observers(c)
+        )
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.parent}, {self.color})"
+
+    def sizeHint(self) -> QSize:
+        """The size hint for the button"""
+        return QSize(20, 20)
+
+    @property
+    def color(self) -> Color:
+        """Returns the current color of the button"""
+        return self._color.color
+
+    @color.setter
+    def color(self, color: Color) -> None:
+        self._color.color = color
+
+    @classmethod
+    def as_tiny(cls, *args, **kwargs):
+        """Makes a tiny push button"""
+        button = cls(*args, **kwargs)
+        button.setBaseSize(20, 20)
+        button.setMinimumWidth(20)
+        button.setMinimumHeight(20)
+        return button
