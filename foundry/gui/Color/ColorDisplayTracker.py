e24acc6609fb58153f7a13a6e26e9bba5b3fe327
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-03-15 09:21:18 -0400

    Update ColorDisplayTracker.py

--- foundry/gui/Color/ColorDisplayTracker.py
+++ foundry/gui/Color/ColorDisplayTracker.py
@@ -9,7 +9,7 @@ from foundry.gui.QWidget.TrackingWidget import TrackingWidget
 from foundry.core.Color.Color import Color
 
 
-class ColorPickerButton(TrackingWidget, ColorDisplayWidget):
+class ColorDisplayTracker(TrackingWidget, ColorDisplayWidget):
     """A colored button that records when it is pressed"""
     def __init__(self, parent: Optional[QWidget], color: Color):
         super().__init__(parent, color)
