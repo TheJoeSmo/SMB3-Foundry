42a17964839f9db6c31cafca127c72f96a7d226f
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-03-15 06:23:07 -0400

    Update PaletteDisplayerWidget.py

--- foundry/gui/Palette/PaletteDisplayerWidget.py
+++ foundry/gui/Palette/PaletteDisplayerWidget.py
@@ -13,7 +13,11 @@ from foundry.gui.Color.ColorDisplayWidget import ColorDisplayWidget as ColorWidg
 
 class PaletteDisplayerWidget(QWidget):
     """A widget to display a single palette"""
-    def __init__(self, parent: Optional[QWidget], palette: Palette) -> None:
+
+    # The text that will be displayed as a tool tip
+    whats_this_text = "<b>Palette Displayer</b><br>Displays the colors of the palette<br/>"
+
+    def __init__(self, parent: Optional[QWidget], palette: Palette, full=False) -> None:
         super().__init__(parent)
         self._palette = ObservablePalette.from_palette(palette)
 
@@ -23,22 +27,20 @@ class PaletteDisplayerWidget(QWidget):
         hbox = QHBoxLayout()
         hbox.setContentsMargins(0, 0, 0, 0)
         hbox.setSpacing(0)
-        for idx in range(3):
-            button = ColorWidget.as_tiny(self, self.palette[idx + 1])
+        for idx in range(4 if full else 3):
+            button = self._load_button(idx, full)
             # Connect the button to update automatically whenever the palette updates
             self.update_observable.attach_observer(lambda pal, i=idx, b=button: setattr(b, "color", pal[i]))
             hbox.addWidget(button)
         self.setLayout(hbox)
 
-        self.setWhatsThis(
-            "<b>Palette Editor</b>"
-            "<br/>"
-            "Edits a group of 3 colors"
-            "<br/>"
-        )
+        self.setWhatsThis(self.whats_this_text)
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.parent}, {self.palette})"
+
+    def _load_button(self, idx: int, full: bool) -> QWidget:
+        return ColorWidget.as_tiny(self, self.palette[idx if full else idx + 1])
 
     @property
     def palette(self) -> Palette:
