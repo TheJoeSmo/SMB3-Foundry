7ef50993f02a78835dc142f7330033301d7348fb
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-03-15 09:20:46 -0400

    Create ColorDisplayTracker.py

---
+++ foundry/gui/Color/ColorDisplayTracker.py
@@ -0,0 +1,15 @@
+
+
+from typing import Optional
+from PySide2.QtWidgets import QWidget
+
+from foundry.gui.Color.ColorDisplayWidget import ColorDisplayWidget
+from foundry.gui.QWidget.TrackingWidget import TrackingWidget
+
+from foundry.core.Color.Color import Color
+
+
+class ColorPickerButton(TrackingWidget, ColorDisplayWidget):
+    """A colored button that records when it is pressed"""
+    def __init__(self, parent: Optional[QWidget], color: Color):
+        super().__init__(parent, color)
