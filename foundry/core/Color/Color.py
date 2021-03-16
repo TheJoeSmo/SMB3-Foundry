45183a16f9903dc2bd467c22f404e413b3a5ecb6
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-03-16 14:06:05 -0400

    Update Color.py

--- foundry/core/Color/Color.py
+++ foundry/core/Color/Color.py
@@ -16,6 +16,10 @@ class Color:
     def __str__(self) -> str:
         return f"{self.red}, {self.green}, {self.blue}"
 
+    @classmethod
+    def from_color(cls, color: "Color"):
+        return cls(color.red, color.green, color.blue)
+
     @property
     def nes_index(self) -> Optional[int]:
         """Returns the estimated index of the color in terms of the NES palette"""
