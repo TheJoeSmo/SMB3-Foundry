0bc0e98610f5da7e3dd6bb034f9e063402567f5b
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-03-17 11:57:33 -0400

    Update PaletteSet.py

--- foundry/core/PaletteSet/PaletteSet.py
+++ foundry/core/PaletteSet/PaletteSet.py
@@ -16,11 +16,6 @@ class PaletteSet:
     def __str__(self) -> str:
         return f"({self[0]}),({self[1]}),({self[2]}),({self[3]})"
 
-    @property
-    def nes_str(self) -> str:
-        """Defines the palette as a NES palette string"""
-        return f"{self[0].nes_str},{self[1].nes_str},{self[2].nes_str},{self[3].nes_str}"
-
     def __getitem__(self, item: int) -> Palette:
         if item == 0:
             return self.palette_0
@@ -44,3 +39,12 @@ class PaletteSet:
             self.palette_3 = value
         else:
             raise NotImplementedError
+
+    @classmethod
+    def from_palette_set(cls, palette_set: "PaletteSet"):
+        cls(palette_set.palette_0, palette_set.palette_1, palette_set.palette_2, palette_set.palette_3)
+
+    @property
+    def nes_str(self) -> str:
+        """Defines the palette as a NES palette string"""
+        return f"{self[0].nes_str},{self[1].nes_str},{self[2].nes_str},{self[3].nes_str}"
