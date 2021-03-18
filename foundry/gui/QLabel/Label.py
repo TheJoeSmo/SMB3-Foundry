03732992e7f56f9daaa88c3a8da39023fe42e5b5
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-03-18 12:22:25 -0400

    Create Label

---
+++ foundry/gui/QLabel/Label.py
@@ -0,0 +1,15 @@
+
+
+from typing import Optional
+from PySide2.QtWidgets import QWidget, QLabel, QSizePolicy
+
+
+class Label(QLabel):
+    """A generic spinner with extended functionality"""
+
+    @classmethod
+    def as_regular(cls, parent: Optional[QWidget], text: str):
+        label = QLabel(parent, text)
+        label.setSizePolicy(QSizePolicy.Preferred)
+        label.setContentsMargins(0, 0, 0, 0)
+        return label
