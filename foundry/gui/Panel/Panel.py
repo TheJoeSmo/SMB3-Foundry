dc9dc54cc67643e228f25bf0eccba074d31c770e
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-03-18 12:22:37 -0400

    Create Panel

---
+++ foundry/gui/Panel/Panel.py
@@ -0,0 +1,20 @@
+
+
+from typing import Optional
+from PySide2.QtWidgets import QWidget, QFormLayout, QSizePolicy
+
+from foundry.gui.QLabel.Label import Label
+
+
+class Panel(QWidget):
+    """A panel that inherits the widgets actions"""
+    def __init__(self, parent: Optional[QWidget], name: str, widget: QWidget):
+        super().__init__(parent)
+
+        self.setSizePolicy(QSizePolicy.Preferred)
+        self.setContentsMargins(0, 0, 0, 0)
+
+        layout = QFormLayout()
+        layout.setContentsMargins(1, 0, 1, 0)
+        layout.addRow(Label.as_regular(self, name), widget)
+        self.setLayout(layout)
