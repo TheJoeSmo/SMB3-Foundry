7d7ebe1a3265d39d6bcafec2ef06c554b4febc14
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-03-19 11:14:16 -0400

    Update ObservableSize.py

--- foundry/core/Size/ObservableSize.py
+++ foundry/core/Size/ObservableSize.py
@@ -8,32 +8,34 @@ class ObservableSize(Size):
     """A size that is observable"""
 
     def __init__(self, width: int, height: int):
-        self._size = Size(width, height)
+        self._width, self._height = width, height
         self.update_observable = GenericObservable("update")
         self.size_update_observable = GenericObservable("size_update")
         self.size_update_observable.attach_observer(lambda *_: self.update_observable.notify_observers())
 
     @property
     def width(self) -> int:
-        return self._size.width
+        return self._width
 
     @width.setter
     def width(self, width: int) -> None:
-        self.size = Size(width, self.height)
+        self._width = width
+        self.size_update_observable.notify_observers(self.size)
 
     @property
     def height(self) -> int:
-        return self._size.height
+        return self._height
 
     @height.setter
-    def height(self, height: int)-> None:
-        self.size = Size(self.width, height)
-    
+    def height(self, height: int) -> None:
+        self._height = height
+        self.size_update_observable.notify_observers(self.size)
+
     @property
     def size(self) -> Size:
         return Size(self.width, self.height)
 
     @size.setter
     def size(self, size: Size) -> None:
-        self._size = size
+        self._width, self._height = size.width, size.height
         self.size_update_observable.notify_observers(self.size)
