b78d292d0d8a088695a390fc3d35ae5e349e718e
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-03-21 12:36:49 -0400

    Update Spinner.py

--- foundry/gui/QSpinner/Spinner.py
+++ foundry/gui/QSpinner/Spinner.py
@@ -3,7 +3,7 @@
 from typing import Optional
 from PySide2.QtWidgets import QSpinBox, QWidget
 
-from foundry.core.Observables.GenericObservable import GenericObservable
+from foundry.core.Observables.SilencerGenericObservable import SilencerGenericObservable
 
 
 class Spinner(QSpinBox):
@@ -12,9 +12,9 @@ class Spinner(QSpinBox):
     def __init__(self, parent: Optional[QWidget], minimum=0, maximum=0xFFFFFF):
         super().__init__(parent)
 
-        self.value_update_observable = GenericObservable("value_update")
+        self.value_update_observable = SilencerGenericObservable("value_update")
         self.valueChanged.connect(lambda *args, **kwargs: self.value_update_observable.notify_observers(*args, **kwargs))
-        self.text_update_observable = GenericObservable("text_update")
+        self.text_update_observable = SilencerGenericObservable("text_update")
         self.textChanged.connect(lambda *args, **kwargs: self.text_update_observable.notify_observers(*args, **kwargs))
 
         self.setRange(minimum, maximum)
